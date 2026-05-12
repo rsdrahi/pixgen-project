779d5a972daf5c60b305324295321ef313f4a659
rsdrahi <rsdrahi13@gmail.com>
2026-05-12 23:35:58 +0600

    added proxy.js file for protect routes.

--- src/proxy.js
+++ src/proxy.js
@@ -1,14 +1,25 @@
 import dns from 'node:dns';
 dns.setServers(['8.8.8.8', '8.8.4.4']);
 import { NextResponse } from 'next/server'
+import { auth } from './lib/auth';
+import { headers } from 'next/headers';
  
  
 // This function can be marked `async` if using `await` inside
-export function proxy(request) {
-  console.log("Message from Proxy");
-  // return NextResponse.redirect(new URL('/home', request.url))
+export async function proxy(request) {
+  
+
+  const session = await auth.api.getSession({
+    headers: await headers()
+  })
+
+  if (!session) {
+    return NextResponse.redirect(new URL('/signin', request.url))
+  }
+
+  
 }
  
 export const config = {
-  matcher: ['/profile'],
+  matcher: ['/profile', "/all-photos/:path"],
 }
